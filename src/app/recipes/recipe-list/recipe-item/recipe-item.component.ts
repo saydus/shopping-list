a7a4c9e12ef7e083f02e59267f08ab6f5880e7ba
saydus <saydus07@gmail.com>
2020-08-27 01:32:22 -0500

    Added dropdown component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from '../../Recipe.model';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
